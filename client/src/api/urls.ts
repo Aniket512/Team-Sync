eb61bbd0e2bc56779980291a2c6cd4488811f17d
Aniket512 <aniketmishra2709@gmail.com>
2024-03-25 20:04:51 +0530

    url

--- client/src/api/urls.ts
+++ client/src/api/urls.ts
@@ -1,10 +1,10 @@
 import { getAccessToken } from '../configs/auth';
 
 export const BASE_URL =
-  process.env.BASE_URL || 'http://localhost:5000'
+  process.env.BASE_URL || 'https://team-sync-api.vercel.app'
 
 export const API_URL =
-  process.env.API_URL || 'http://localhost:5000/api';
+  process.env.API_URL || 'https://team-sync-api.vercel.app/api';
 
 export function loginUrl() {
   return `${BASE_URL}/auth/login`;
